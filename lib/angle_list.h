28a473bded7b718d0eab4e5fe33311e64c1950da
malramsay64 <malramsay64@gmail.com>
2014-12-17 17:01:29 +1100

    Angle output in radians

--- lib/angle_list.h
+++ lib/angle_list.h
@@ -59,7 +59,7 @@ public:
     int print(std::ofstream * file){
         if (dist.size() == a.size()){
             for (int i = 0; i < a.size(); i++){
-                *file << (a.at(i).get_mean())*180/PI << " " << dist.at(i).get_mean() << " " << a.at(i).get_count() << std::endl;
+                *file << (a.at(i).get_mean()) << " " << dist.at(i).get_mean() << " " << a.at(i).get_count() << std::endl;
             }
         }
         else {
